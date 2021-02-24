bbe383836b09a09da7e9e5f0d0d953cd971fb2a5
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-02-24 13:34:38 +0200

    Initial commit

---
+++ src/main/java/ua/edu/kneu/irbisapi/IrbisApiApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.kneu.irbisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrbisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrbisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/edu/kneu/irbisapi/IrbisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.kneu.irbisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrbisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
