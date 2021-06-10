a7673c8eeee58acb25d6aabe30b9ca537365c92f
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-06-10 22:03:50 +0300

    AuthorService: filter authors by workPlace; AuthorMapper: refactor links object building

--- src/main/java/ua/edu/kneu/irbisapi/dtl/work/WorkDTO.java
+++ src/main/java/ua/edu/kneu/irbisapi/dtl/work/WorkDTO.java
@@ -1,13 +1,17 @@
 package ua.edu.kneu.irbisapi.dtl.work;
 
+import java.util.List;
+
 public class WorkDTO {
     public final int id;
     public final String authorName;
     public final String title;
+    public final List<WorkDTO> content;
 
-    public WorkDTO(int id, String authorName, String title) {
+    public WorkDTO(int id, String authorName, String title, List<WorkDTO> content) {
         this.id = id;
         this.authorName = authorName;
         this.title = title;
+        this.content = content;
     }
 }
--- src/main/java/ua/edu/kneu/irbisapi/dtl/work/WorkMapper.java
+++ src/main/java/ua/edu/kneu/irbisapi/dtl/work/WorkMapper.java
@@ -4,10 +4,12 @@ import org.springframework.stereotype.Component;
 import ru.arsmagna.MarcRecord;
 import ua.edu.kneu.irbisapi.dtl.IRecordMapper;
 
+import java.util.ArrayList;
+
 @Component
 public class WorkMapper implements IRecordMapper<WorkDTO> {
     @Override
     public WorkDTO map(MarcRecord record) {
-        return new WorkDTO(record.mfn, record.fm(700, 'a'), record.fm(200, 'a'));
+        return new WorkDTO(record.mfn, record.fm(700, 'a'), record.fm(200, 'a'), new ArrayList<WorkDTO>()); //TODO: map content from record.fma(330)
     }
 }
