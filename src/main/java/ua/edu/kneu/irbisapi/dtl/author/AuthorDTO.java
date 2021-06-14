aeee3574389f84cac6c83e5564830fe658246dfb
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-06-14 12:47:38 +0300

    AuthorMapper: map academic status and degree of author to AuthorDTO

--- src/main/java/ua/edu/kneu/irbisapi/dtl/author/AuthorDTO.java
+++ src/main/java/ua/edu/kneu/irbisapi/dtl/author/AuthorDTO.java
@@ -7,13 +7,18 @@ public final class AuthorDTO {
     public final String firstName;
     public final String lastName;
     public final String workPlace;
+    public final String academicStatus;
+    public final String degree;
     public final Map<String, String> links;
 
-    public AuthorDTO(int id, String firstName, String lastName, String workPlace, Map<String, String> links) {
+    public AuthorDTO(int id, String firstName, String lastName, String workPlace, String academicStatus, String degree,
+                     Map<String, String> links) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.workPlace = workPlace;
+        this.academicStatus = academicStatus;
+        this.degree = degree;
         this.links = links;
     }
 
