011eb8d178417f2348d94e84ba3770ec07ac4593
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2021-06-15 10:32:07 +0300

    AuthorMapper: fix exception when there is no 340 field

--- src/main/java/ua/edu/kneu/irbisapi/dtl/author/AuthorMapper.java
+++ src/main/java/ua/edu/kneu/irbisapi/dtl/author/AuthorMapper.java
@@ -20,6 +20,9 @@ public class AuthorMapper implements IRecordMapper<AuthorDTO> {
                     final String linkHref = link.getFirstSubFieldValue('i');
                     return linkHref != null ? linkHref : "";
                 }));
-        return new AuthorDTO(record.mfn, record.fm(210, 'g'), record.fm(210, 'a'), record.fm(910, 'p'), record.getField(340)[0].getFirstSubFieldValue('A'), record.getField(340)[1].getFirstSubFieldValue('A'), links);
+        final String academicStatus = (record.getField(340).length > 0 ? record.getField(340)[0].getFirstSubFieldValue('A') : null);
+        final String degree = (record.getField(340).length > 1 ? record.getField(340)[1].getFirstSubFieldValue('A') : null);
+        System.out.println(record.mfn);
+        return new AuthorDTO(record.mfn, record.fm(210, 'g'), record.fm(210, 'a'), record.fm(910, 'p'), academicStatus, degree, links);
     }
 }
