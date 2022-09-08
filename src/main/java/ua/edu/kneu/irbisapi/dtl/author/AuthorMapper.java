12fc8d12af26bdf98df668724fe052b3bdc98f70
Vadim Shesterikov <vadim.a.shesterikov@gmail.com>
2022-09-08 22:04:51 +0300

    AuthorMapper: add logging

--- src/main/java/ua/edu/kneu/irbisapi/dtl/author/AuthorMapper.java
+++ src/main/java/ua/edu/kneu/irbisapi/dtl/author/AuthorMapper.java
@@ -1,10 +1,12 @@
 package ua.edu.kneu.irbisapi.dtl.author;
 
+import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Component;
 import ru.arsmagna.MarcRecord;
 import ua.edu.kneu.irbisapi.dtl.IRecordMapper;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -12,14 +14,20 @@ import java.util.stream.Collectors;
 public class AuthorMapper implements IRecordMapper<AuthorDTO> {
     @Override
     public AuthorDTO map(MarcRecord record) {
-        Map<String, String> links = Arrays.stream(record.getField(951)).collect(Collectors.toMap(link -> {
-                    final String linkName = link.getFirstSubFieldValue('t');
-                    return linkName != null ? linkName : "Link";
-                },
-                link -> {
-                    final String linkHref = link.getFirstSubFieldValue('i');
-                    return linkHref != null ? linkHref : "";
-                }));
+        Map<String, String> links = new HashMap<>();
+        try {
+            links = Arrays.stream(record.getField(951)).collect(Collectors.toMap(link -> {
+                final String linkName = link.getFirstSubFieldValue('t');
+                return linkName != null ? linkName : "Link";
+            },
+            link -> {
+                final String linkHref = link.getFirstSubFieldValue('i');
+                return linkHref != null ? linkHref : "";
+            }));
+        } catch(IllegalStateException e) {
+            LogFactory.getLog(AuthorMapper.class).error(record.mfn + " " + e.getMessage());
+        }
+        
         final int id = record.mfn;
         final String firstName = record.fm(210, 'g');
         final String lastName = record.fm(210, 'a');
